70837e2ce9ee1dc47c9a4f5971cbbc8d35b3ef37
MD RASEL HOSSEN (RaselHossen0) <hrasel2002@gmail.com>
2026-01-26 22:25:55 +0600

    fix: add missing MDX components to fix build errors
    - Added InfoPageHero, InfoAccordion, PartnersHero, LetsConnect
    - Added InfoPageTwo, MissionSection, OurStory, WeStandFor, WhereWeWork
    - Added FaBug, FaExclamationTriangle, FaFlag, FaUserTimes icons
    - Fixes build errors for /about, /pages/payment-methods, /pages/report-a-problem, /safe-trading-guide

--- src/components/MDX-Component/mdx-components.js
+++ src/components/MDX-Component/mdx-components.js
@@ -1,9 +1,32 @@
 import OurBlogs from "../LandingPage/OurBlogs";
 import QuickAnswers from "../LandingPage/QuickAnswers";
 import WorkProcess from "../LandingPage/WorkProcess";
+import InfoPageHero from "../InfoPage/InfoPageHero";
+import InfoAccordion from "../InfoPage/InfoAccordian";
+import PartnersHero from "../InfoPage/PartnersHero";
+import LetsConnect from "../InfoPage/LetsConnect";
+import InfoPageTwo from "../InfoPage/InfoPageHeroTwo";
+import MissionSection from "../InfoPage/OurMission";
+import OurStory from "../InfoPage/OurStory";
+import WeStandFor from "../InfoPage/WeStandFor";
+import WhereWeWork from "../InfoPage/WhereWeWork";
+import { FaBug, FaExclamationTriangle, FaFlag, FaUserTimes } from "react-icons/fa";
 
 export const mdxComponents = {
   WorkProcess,
   OurBlogs,
   QuickAnswers,
+  InfoPageHero,
+  InfoAccordion,
+  PartnersHero,
+  LetsConnect,
+  InfoPageTwo,
+  MissionSection,
+  OurStory,
+  WeStandFor,
+  WhereWeWork,
+  FaBug,
+  FaExclamationTriangle,
+  FaFlag,
+  FaUserTimes,
 };
